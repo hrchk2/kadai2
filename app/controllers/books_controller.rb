12b19ee7a60f5d924bc20b86b13821e7d1dd56e4
EC2 Default User <amrumineq@gmail.com>
2022-10-08 06:36:16 +0000

    update機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,27 +5,34 @@ class BooksController < ApplicationController
   def create
     @book =Book.new(book_params)
     @book.save
-    redirect_to "/index/:id"
+    render("books/show")
   end
     
   def index
-    @books =Book.all
+    @books =Book.all.order(id: :asc)
   end
   
   def show
-    @book = Book.find(params["id"])
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
   
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+
   def destroy
   end
  
   private
   
   def book_params
-  params.permit(:title, :body)
+  params.require(:book).permit(:title, :body)
   end
 
 end
